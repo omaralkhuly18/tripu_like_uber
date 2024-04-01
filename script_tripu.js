a37bbd16d9a1cf572a9cbec2f8c77addda6f3368
omaralkhuly18 <omaralkhuly18@gmail.com>
2024-04-01 14:55:48 +0200

    first_trip_uber

---
+++ script_tripu.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "50%";
+    document.getElementById("myNav").style.left = "50%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+    document.getElementById("myNav").style.left = "0%";
+  }
